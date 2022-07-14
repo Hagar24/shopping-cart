beae291ac6d66dfbd47ce070fb85226cf325236f
Hagar24 <98018955+Hagar24@users.noreply.github.com>
2022-07-14 17:17:12 +0200

    Add files via upload

---
+++ shopping-cart/src/Data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id:"hager",
+    name:"casual shirt",
+    price:"55",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/img-1.jpg"
+},
+{    id:"anime",
+name:"office shirt",
+price:"120",
+desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+img:"images/img-2.jpg"
+},
+{    id:"sakura",
+name:"T-shirt",
+price:"30",
+desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+img:"images/img-3.jpg"
+},
+{    id:"yourname",
+name:"mens suit",
+price:"200",
+desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+img:"images/img-4.jpg"
+}];
